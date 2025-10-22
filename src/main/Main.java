482d7ce35f1f88487d9f11392607019293df858e
PrecursorVT <precursorvt@gmail.com>
2025-10-22 12:31:22 +0200

    quitando cosas feas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
-import javax.swing.SwingUtilities;
 
-import cliente.Cliente;
 import vista.VentanaChat;
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new VentanaChat().setVisible(true);
-        });
-    }
+	public static void main(String[] args) {
+        VentanaChat ventana = new VentanaChat();
+        ventana.setVisible(true);
+	}
+
 }
